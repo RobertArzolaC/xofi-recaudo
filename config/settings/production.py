dfabe839e16d301ac12d3907d045f4bd2b6a0fc4
Robert Arzola Castillo <RobertArzolaC>
2025-11-26 11:15:22 -0500

    fix(settings): cambiar el backend de almacenamiento de archivos estáticos a ManifestStaticFilesStorage

--- config/settings/production.py
+++ config/settings/production.py
@@ -142,7 +142,7 @@ STORAGES = {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "django.contrib.staticfiles.storage.ManifestStaticFilesStorage",
     },
 }
 
